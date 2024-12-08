35b7959c1aecb2f7016b94388040806601367116
Burhan Ali Shaikh <85751219+BurhanShaikh-9@users.noreply.github.com>
2024-12-08 20:40:17 +0500

    update

--- portfolioV4/portfolio/blog_app/src/services/constants.js
+++ portfolioV4/portfolio/blog_app/src/services/constants.js
@@ -1,1 +1,2 @@
-export const baseUrl = 'http://192.168.100.14:5000/api'
+// export const baseUrl = 'http://192.168.100.14:5000/api'
+export const baseUrl = 'https://blogappbackend-mu.vercel.app/api'
